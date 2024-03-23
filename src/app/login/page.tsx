077de030f58844161cdecab95f63c593878e8e02
koki-table <koki.s0327@icloud.com>
2024-03-23 09:14:38 +0900

    feat: login画面作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,5 @@
-import { login, signup } from './actions'
+import { LoginPage } from './login-page'
 
-export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor='email'>Email:</label>
-      <input id='email' name='email' type='email' required />
-      <label htmlFor='password'>Password:</label>
-      <input id='password' name='password' type='password' required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+export default function Page() {
+  return <LoginPage />
 }
